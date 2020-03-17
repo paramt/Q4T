6654f689411470afa57664f59e05d80835ec95b3
Param Thakkar <pht0104@gmail.com>
2020-03-17 10:12:17 -0400

    Add credentials variable to configure.py

--- src/configure.py
+++ src/configure.py
@@ -3,5 +3,6 @@ token = ""
 channel = ""
 
 # Google Sheets
-spreadsheet = ""        # Name of Google Sheet
-options = 4             # Number of options
+spreadsheet = ""    # Name of Google Sheet
+options = 4         # Number of options
+credentials = ""    # Path to credentials JSON file
