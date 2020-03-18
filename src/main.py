48fe72129d7e0ab7ee33c3d342be3632371c653d
Param Thakkar <pht0104@gmail.com>
2020-03-18 11:57:14 -0400

    Shuffle options randomly

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import random
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
@@ -16,12 +17,15 @@ index = gc.open(configure.spreadsheet).worksheet("index")
 index_val = index.cell(1, 1).value
 question = datasheet.cell(index_val, 1).value
 options = [datasheet.cell(index_val, i + 2).value for i in range(10)]
-correct = options[0]
+answer = options[0]
 options = [option for option in options if option] # Remove blank questions
+random.shuffle(options)
+answer_index = options.index(answer)
 
 url = f"https://api.telegram.org/bot{configure.token}/sendPoll"
 
-query = {"chat_id":f"@{configure.channel}", "question":question, "type":"quiz", "correct_option_id":"0"}
+query = {"chat_id": f"@{configure.channel}", "question": question,
+		 "type":"quiz", "correct_option_id": answer_index}
 response = requests.request("GET", url, params = query, json = {"options": options})
 
 if response.json()["ok"] == True:
