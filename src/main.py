6322f9265492cdf5995a5d6f4a635dc576a944ec
Param Thakkar <pht0104@gmail.com>
2020-03-23 12:35:59 -0400

    Rename configure.py to config.py

--- src/main.py
+++ src/main.py
@@ -2,29 +2,29 @@ import random
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
-import configure
+import config
 import reset
 
 # Authorize Google Sheets
 try:
 	scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-	credentials = ServiceAccountCredentials.from_json_keyfile_name(configure.credentials, scope)
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(config.credentials, scope)
 	gc = gspread.authorize(credentials)
 except OSError as e:
 	print("JSON file with Google account credentials not found!")
-	print("Make sure you've followed the README instructions and added the filepath of your credentials file to configure.py")
+	print("Make sure you've followed the README instructions and added the filepath of your credentials file to config.py")
 	exit(1)
 
 # Open sheets
-datasheet = gc.open(configure.spreadsheet).worksheet("datasheet")
-index = gc.open(configure.spreadsheet).worksheet("index")
+datasheet = gc.open(config.spreadsheet).worksheet("datasheet")
+index = gc.open(config.spreadsheet).worksheet("index")
 
 # Get question and options from current index
 index_val = index.cell(1, 1).value
 question = datasheet.cell(index_val, 1).value
 
 if question == "":
-	if configure.loop:
+	if config.loop:
 		reset.reset_index()
 		index_val = index.cell(1, 1).value
 		question = datasheet.cell(index_val, 1).value
@@ -38,9 +38,9 @@ options = [option for option in options if option] # Remove blank questions
 random.shuffle(options)
 answer_index = options.index(answer)
 
-url = f"https://api.telegram.org/bot{configure.token}/sendPoll"
+url = f"https://api.telegram.org/bot{config.token}/sendPoll"
 
-query = {"chat_id": f"@{configure.channel}", "question": question,
+query = {"chat_id": f"@{config.channel}", "question": question,
 		 "type":"quiz", "correct_option_id": answer_index}
 response = requests.request("GET", url, params = query, json = {"options": options})
 
--- src/reset.py
+++ src/reset.py
@@ -1,25 +1,25 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import re
-import configure
+import config
 
 # Authorize Google Sheets
 try:
 	scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-	credentials = ServiceAccountCredentials.from_json_keyfile_name(configure.credentials, scope)
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(config.credentials, scope)
 	gc = gspread.authorize(credentials)
 except OSError:
 	print("JSON file with Google account credentials not found!")
-	print("Make sure you've followed the README instructions and added the filepath of your credentials file to configure.py")
+	print("Make sure you've followed the README instructions and added the filepath of your credentials file to config.py")
 	exit(1)
 
 # Reset index to 2
 def reset_index():
-	gc.open(configure.spreadsheet).worksheet("index").update("A1", 2)
+	gc.open(config.spreadsheet).worksheet("index").update("A1", 2)
 
 # Reset datasheet
 def reset_datasheet():
-	datasheet = gc.open(configure.spreadsheet).worksheet("datasheet")
+	datasheet = gc.open(config.spreadsheet).worksheet("datasheet")
 	cells_to_delete = datasheet.range("A2:Z1000")
 	for cell in cells_to_delete: cell.value = ""
 	datasheet.update_cells(cells_to_delete)
